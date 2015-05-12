dd1a7ccc31f8bb81be9c9803fbe24a5154e51b90
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-12 15:12:34 +0100

    Added function to calculate the winner of the game and display it

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,15 @@
 console.log('Javascipt is working');
 
 var lastMove = '';
+var table = [];
+var indexPosition;
 
 function getMove() {
   if (lastMove === 'x') {
+    $('#move').text("User2, it's your move!");
     getUser2Move();
   } else {
+    $('#move').text("User1, it's your move!");
     getUser1Move();
   }
 }
@@ -15,7 +19,9 @@ function getUser2Move() {
     $(this).removeClass('cross');
     $(this).addClass('nought');
     lastMove = 'o';
-    getMove();
+    indexPosition = Number($(this).attr('value')) - 1;
+    table[indexPosition] = 'o';
+    checkWinner();
   });
 }
 
@@ -24,8 +30,35 @@ function getUser1Move() {
     $(this).removeClass('nought');
     $(this).addClass('cross');
     lastMove = 'x';
-    getMove();
+    indexPosition = Number($(this).attr('value')) - 1;
+    table[indexPosition] = 'x';
+    checkWinner();
   })
+}
+
+
+function checkWinner(lastMove) {
+  var winner = "";
+  if (table[0] === table[1] && table[1] === table[2]) {
+    winner = table[0];
+  } else if (table[3] === table[4] && table[4] === table[5]) {
+    winner = table[3];
+  } else if (table[6] === table[7] && table[7] === table[8]) {
+    winner = table[6];
+  } else if (table[0] === table[4] && table[4] === table[8]) {
+    winner = table[0];
+  } else if (table[2] === table[4] && table[4] === table[6]) {
+    winner = table[2];
+  }
+
+  if (winner === 'x' || winner === 'o') {
+    $('#move').text('');
+    $('#result').text('');
+    $('#result').text("Winner is " + winner);
+  } else {
+    getMove();
+  } 
+
 }
 
 $(document).ready(function() {
