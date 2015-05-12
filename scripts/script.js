0aa584383febb4add567322bd1bde0c016912520
Erika Jonikaite <erikajon@hotmail.co.uk>
2015-05-12 11:58:55 +0100

    Added ability for user1 and user2 to switch turns

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,33 @@
 console.log('Javascipt is working');
+
+var lastMove = '';
+
+function getMove() {
+  if (lastMove === 'x') {
+    getUser2Move();
+  } else {
+    getUser1Move();
+  }
+}
+
+function getUser2Move() {
+  $('li').on('click', function() {
+    $(this).removeClass('cross');
+    $(this).addClass('nought');
+    lastMove = 'o';
+    getMove();
+  });
+}
+
+function getUser1Move() {
+  $('li').on('click', function() {
+    $(this).removeClass('nought');
+    $(this).addClass('cross');
+    lastMove = 'x';
+    getMove();
+  })
+}
+
+$(document).ready(function() {
+  getMove();
+})
